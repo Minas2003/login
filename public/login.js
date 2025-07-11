9f36f6ae786eb965e371d871d8a7f2625f36a2e7
Minas2003 <nahapetyanminas9@gmail.com>
2025-07-12 02:22:15 +0300

    change

--- public/login.js
+++ public/login.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const data = Object.fromEntries(formData.entries());
   
       try {
-        const res = await fetch('/login', {
+        const res = await fetch('login-u8as.onrender.com/login', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(data),
